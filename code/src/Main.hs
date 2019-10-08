6226e273e57dceb69f36701173be01cab1f1c992
rubenpieters <rubenpietersgithub@gmail.com>
2019-10-08 15:28:52 +0200

    basic use cases

--- code/src/Main.hs
+++ code/src/Main.hs
@@ -142,6 +142,14 @@ instance Par (Const Duration) where
 duration :: Const Duration a -> Duration
 duration = getConst
 
+runAnimations :: Float -> obj -> [Animation obj Identity ()] -> (obj, [Animation obj Identity ()])
+runAnimations t w [] = (w, [])
+runAnimations t w (anim:r) = let
+  (newWorld, res, _) = runIdentity (runAnimation anim w t)
+  in case res of
+    Left newAnim -> fmap (\l -> newAnim : l) (runAnimations t newWorld r)
+    Right _ -> runAnimations t newWorld r
+
 type RGB = (Float, Float, Float)
 
 hexToRgb :: String -> String -> String -> RGB
@@ -150,7 +158,6 @@ hexToRgb v1 v2 v3 = let
   g = fst (head (readHex v2))
   b = fst (head (readHex v3))
   in (r / 256, g / 256, b / 256)
-
 
 data Sprite = Sprite
   { _x :: Float
@@ -174,10 +181,173 @@ drawSprite Sprite{ _x, _y, _color, _alpha, _pic, _width, _height } = let
   Translate (_x + _width / 2 - sw / 2) (-_y - _height / 2 + sh / 2)
 
 data MenuWorld = MenuWorld
-  { _menu :: Sprite
+  { _menuBgElements :: [Sprite]
+  , _obscuringBox :: Sprite
+  , _menu :: Sprite
+  , _mnAnimations :: [Animation MenuWorld Identity ()]
+  , _menuOpen :: Bool
   }
 
 makeLenses ''MenuWorld
+
+drawMenuWorld :: MenuWorld -> Picture
+drawMenuWorld w = Pictures $
+  map drawSprite (w ^. menuBgElements) ++
+  [ drawSprite (w ^. obscuringBox)
+  , drawSprite (w ^. menu)
+  ]
+
+menuSlideIn :: (Basic MenuWorld f) => f ()
+menuSlideIn = basic (menu . width) (For 0.5) (To 75)
+
+appFadeOut :: (Basic MenuWorld f) => f ()
+appFadeOut = basic (obscuringBox . alpha) (For 0.5) (To 0.6)
+
+menuAnimation :: (Par f, Basic MenuWorld f) => f ()
+menuAnimation = menuSlideIn `par` appFadeOut
+
+menuAnimationRev :: (Par f, Basic MenuWorld f) => f ()
+menuAnimationRev = par
+  (basic (menu . width) (For 0.5) (To 0))
+  (basic (obscuringBox . alpha) (For 0.5) (To 0))
+
+handleInputMn :: Event -> MenuWorld -> MenuWorld
+handleInputMn (EventKey (MouseButton LeftButton) Down _ (mouseX, mouseY)) w =
+  if w ^. menuOpen
+  then
+    w & mnAnimations .~ [menuAnimationRev]
+      & menuOpen .~ False
+  else
+    w & mnAnimations .~ [menuAnimation]
+      & menuOpen .~ True
+handleInputMn _ w = w
+
+menuPic :: Float -> Float -> Picture
+menuPic width height = let
+  (r, g, b) = hexToRgb "58" "74" "8a"
+  itemColor = Color (makeColor r g b 1)
+  in Pictures
+  [ rectangleSolid width height
+  , rectangleSolid (0.6 * width) (0.1 * height) & Translate (0.05 * width) (-0.30 * height) & itemColor
+  , rectangleSolid (0.6 * width) (0.1 * height) & Translate (0.05 * width) (-0.15 * height) & itemColor
+  , rectangleSolid (0.6 * width) (0.1 * height) & Translate (0.05 * width) 0 & itemColor
+  , rectangleSolid (0.6 * width) (0.1 * height) & Translate (0.05 * width) (0.15 * height) & itemColor
+  , rectangleSolid (0.6 * width) (0.1 * height) & Translate (0.05 * width) (0.30 * height) & itemColor
+  ]
+
+updateMn :: Float -> MenuWorld -> MenuWorld
+updateMn t w = let
+  (newWorld, newAnimations) = runAnimations t w (w ^. mnAnimations)
+  in newWorld & mnAnimations .~ newAnimations
+
+initialMenuWorld :: MenuWorld
+initialMenuWorld = MenuWorld
+  { _menuBgElements =
+      [ Sprite
+        { _x = 0
+        , _y = 0
+        , _width = 100
+        , _height = 100
+        , _color = hexToRgb "c4" "c8" "c2"
+        , _alpha = 1
+        , _pic = rectangleSolid
+        }
+      , Sprite
+        { _x = 0
+        , _y = 0
+        , _width = 100
+        , _height = 28
+        , _color = hexToRgb "55" "7a" "95"
+        , _alpha = 1
+        , _pic = rectangleSolid
+        }
+      , Sprite
+        { _x = 20
+        , _y = 35
+        , _width = 50
+        , _height = 8
+        , _color = hexToRgb "b1" "a2" "96"
+        , _alpha = 1
+        , _pic = rectangleSolid
+        }
+      , Sprite
+        { _x = 20
+        , _y = 45
+        , _width = 60
+        , _height = 30
+        , _color = hexToRgb "b1" "a2" "96"
+        , _alpha = 1
+        , _pic = rectangleSolid
+        }
+      , Sprite
+        { _x = 20
+        , _y = 83
+        , _width = 50
+        , _height = 8
+        , _color = hexToRgb "b1" "a2" "96"
+        , _alpha = 1
+        , _pic = rectangleSolid
+        }
+      , Sprite
+        { _x = 20
+        , _y = 93
+        , _width = 60
+        , _height = 7
+        , _color = hexToRgb "b1" "a2" "96"
+        , _alpha = 1
+        , _pic = rectangleSolid
+        }
+      , Sprite
+        { _x = 7.5
+        , _y = 7
+        , _width = 25
+        , _height = 10
+        , _color = hexToRgb "73" "95" "ae"
+        , _alpha = 1
+        , _pic = rectangleSolid
+        }
+      , Sprite
+        { _x = 37.5
+        , _y = 7
+        , _width = 25
+        , _height = 10
+        , _color = hexToRgb "73" "95" "ae"
+        , _alpha = 1
+        , _pic = rectangleSolid
+        }
+      , Sprite
+        { _x = 67.5
+        , _y = 7
+        , _width = 25
+        , _height = 10
+        , _color = hexToRgb "73" "95" "ae"
+        , _alpha = 1
+        , _pic = rectangleSolid
+        }
+      ]
+  , _menu =
+      Sprite
+      { _x = 0
+      , _y = 0
+      , _width = 0
+      , _height = 100
+      , _color = hexToRgb "31" "55" "70"
+      , _alpha = 1
+      , _pic = menuPic
+      }
+  , _obscuringBox =
+      Sprite
+      { _x = 0
+      , _y = 0
+      , _width = 100
+      , _height = 100
+      , _color = (0, 0, 0)
+      , _alpha = 0
+      , _pic = rectangleSolid
+      }
+  , _mnAnimations = []
+  , _menuOpen = False
+  }
 
 data NavBarWorld = NavBarWorld
   { _navBarBgElements :: [Sprite]
@@ -202,24 +372,28 @@ drawNavBarWorld w = Pictures $
   , drawSprite (w ^. navBarLine2)
   ]
 
-bar1Anim :: (Applicative f, Basic NavBarWorld f) => f ()
-bar1Anim =
-  basic (navBarLine1 . height) (For 0.25) (To 4) *>
-  basic (navBarLine2 . height) (For 0.25) (To 0)
+bar1Animation :: (Applicative f, Basic NavBarWorld f) => f ()
+bar1Animation =
+  basic (navBarLine2 . height) (For 0.25) (To 0) *>
+  basic (navBarLine1 . height) (For 0.25) (To 4)
 
-bar2Anim :: (Applicative f, Basic NavBarWorld f) => f ()
-bar2Anim =
-  basic (navBarLine2 . height) (For 0.25) (To 4) *>
-  basic (navBarLine1 . height) (For 0.25) (To 0)
+bar2Animation :: (Applicative f, Basic NavBarWorld f) => f ()
+bar2Animation = decreaseBar1 `seq` increaseBar2
+
+decreaseBar1 :: (Basic NavBarWorld f) => f ()
+decreaseBar1 = basic (navBarLine1 . height) (For 0.25) (To 0)
+
+increaseBar2 :: (Basic NavBarWorld f) => f ()
+increaseBar2 = basic (navBarLine2 . height) (For 0.25) (To 4)
 
 handleInputNb :: Event -> NavBarWorld -> NavBarWorld
 handleInputNb (EventKey (MouseButton LeftButton) Down _ (mouseX, mouseY)) w =
   if w ^. barSelected == 1
   then
-    w & nbAnimations .~ [bar2Anim]
+    w & nbAnimations .~ [bar2Animation]
       & barSelected .~ 2
   else
-    w & nbAnimations .~ [bar1Anim]
+    w & nbAnimations .~ [bar1Animation]
       & barSelected .~ 1
 handleInputNb _ w = w
 
@@ -227,14 +401,6 @@ updateNb :: Float -> NavBarWorld -> NavBarWorld
 updateNb t w = let
   (newWorld, newAnimations) = runAnimations t w (w ^. nbAnimations)
   in newWorld & nbAnimations .~ newAnimations
-
-runAnimations :: Float -> obj -> [Animation obj Identity ()] -> (obj, [Animation obj Identity ()])
-runAnimations t w [] = (w, [])
-runAnimations t w (anim:r) = let
-  (newWorld, res, _) = runIdentity (runAnimation anim w t)
-  in case res of
-    Left newAnim -> fmap (\l -> newAnim : l) (runAnimations t newWorld r)
-    Right _ -> runAnimations t newWorld r
 
 initialNavBarWorld :: NavBarWorld
 initialNavBarWorld = NavBarWorld
@@ -356,17 +522,53 @@ fetchInbetweens delta k obj (Animation f) acc = let
      Left anim -> fetchInbetweens delta (k - 1) obj' anim (acc ++ [obj])
      Right _ -> acc ++ [obj, obj']
 
-navBarPic1 :: [NavBarWorld]
-navBarPic1 = let
+usecase1fig :: [MenuWorld]
+usecase1fig = let
+  k = 3
+  anim :: (Par f, Basic MenuWorld f) => f ()
+  anim = menuAnimation
+  in fetchInbetweens (getDuration (duration anim) / fromIntegral k) k initialMenuWorld anim []
+
+usecase1basic1 :: [MenuWorld]
+usecase1basic1 = let
+  k = 3
+  anim :: (Basic MenuWorld f) => f ()
+  anim = menuSlideIn
+  in fetchInbetweens (getDuration (duration anim) / fromIntegral k) k initialMenuWorld anim []
+
+usecase1basic2 :: [MenuWorld]
+usecase1basic2 = let
+  k = 3
+  anim :: (Basic MenuWorld f) => f ()
+  anim = appFadeOut
+  in fetchInbetweens (getDuration (duration anim) / fromIntegral k) k initialMenuWorld anim []
+
+usecase2fig :: [NavBarWorld]
+usecase2fig = let
   k = 3
   anim :: (Applicative f, Basic NavBarWorld f) => f ()
-  anim = bar2Anim
+  anim = bar2Animation
   in fetchInbetweens (getDuration (duration anim) / fromIntegral k) k initialNavBarWorld anim []
 
-drawnWorlds :: [NavBarWorld] -> Picture
-drawnWorlds worlds = let
+usecase2basic1 :: [NavBarWorld]
+usecase2basic1 = let
+  k = 3
+  anim :: (Basic NavBarWorld f) => f ()
+  anim = decreaseBar1
+  in fetchInbetweens (getDuration (duration anim) / fromIntegral k) k initialNavBarWorld anim []
+
+usecase2basic2 :: [NavBarWorld]
+usecase2basic2 = let
+  k = 3
+  anim :: (Applicative f, Basic NavBarWorld f) => f ()
+  anim = increaseBar2
+  world = initialNavBarWorld & navBarLine1 . height .~ 0
+  in fetchInbetweens (getDuration (duration anim) / fromIntegral k) k world anim []
+
+drawnWorlds :: (obj -> Picture) -> [obj] -> Picture
+drawnWorlds drawFun worlds = let
   k = length worlds
-  f (i, w) = drawNavBarWorld w & Translate (50 - 206 + 104 * i) 0
+  f (i, w) = drawFun w & Translate (50 - 206 + 104 * i) 0
   whiteBoxTop i = rectangleSolid 5 5 & Color (makeColor 1 1 1 1) & Translate (-203 + 10 * i) (-55)
   whiteBoxBot i = rectangleSolid 5 5 & Color (makeColor 1 1 1 1) & Translate (-203 + 10 * i) 55
   in Pictures (map f (zip [0..] worlds) ++ map whiteBoxTop [0..50] ++ map whiteBoxBot [0..50])
@@ -382,5 +584,12 @@ main = let
   x :: Int <- readLn
   case x of
     1 -> play window bgColor 60 initialNavBarWorld drawNavBarWorld handleInputNb updateNb
-    2 -> exportPictureToPNG (sw * 4 + 4 * 3, sh + 20) bgColor "test.png" (drawnWorlds navBarPic1)
+    2 -> play window bgColor 60 initialMenuWorld drawMenuWorld handleInputMn updateMn
+    9 -> do
+      exportPictureToPNG (sw * 4 + 4 * 3, sh + 20) bgColor "../paper/pictures/usecase1fig.png" (drawnWorlds drawMenuWorld usecase1fig)
+      exportPictureToPNG (sw * 4 + 4 * 3, sh + 20) bgColor "../paper/pictures/usecase1basic1.png" (drawnWorlds drawMenuWorld usecase1basic1)
+      exportPictureToPNG (sw * 4 + 4 * 3, sh + 20) bgColor "../paper/pictures/usecase1basic2.png" (drawnWorlds drawMenuWorld usecase1basic2)
+      exportPictureToPNG (sw * 4 + 4 * 3, sh + 20) bgColor "../paper/pictures/usecase2fig.png" (drawnWorlds drawNavBarWorld usecase2fig)
+      exportPictureToPNG (sw * 4 + 4 * 3, sh + 20) bgColor "../paper/pictures/usecase2basic1.png" (drawnWorlds drawNavBarWorld usecase2basic1)
+      exportPictureToPNG (sw * 4 + 4 * 3, sh + 20) bgColor "../paper/pictures/usecase2basic2.png" (drawnWorlds drawNavBarWorld usecase2basic2)
     _ -> error "unknown"
