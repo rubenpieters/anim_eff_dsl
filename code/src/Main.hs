e4e64080b39d09a582c6cbf0c725d4deb127a7e1
rubenpieters <rubenpietersgithub@gmail.com>
2019-10-09 16:14:24 +0200

    init particle example, small additions paper

--- code/src/Main.hs
+++ code/src/Main.hs
@@ -15,15 +15,20 @@ import Numeric
 import Control.Applicative (liftA2)
 import Control.Monad
 import Control.Monad.Identity
+import Control.Monad.IO.Class
 
 import Data.Functor.Const
+import Data.List (find, findIndex, nub)
 
 import Lens.Micro
 import Lens.Micro.TH
 
 import Graphics.Gloss hiding (color)
 import Graphics.Gloss.Interface.Pure.Game hiding (color)
+import Graphics.Gloss.Interface.IO.Game hiding (color)
 import Graphics.Gloss.Export.PNG
+
+import System.Random
 
 newtype Duration = For { getDuration :: Float }
   deriving (Ord, Eq, Show)
@@ -150,6 +155,28 @@ runAnimations t w (anim:r) = let
     Left newAnim -> fmap (\l -> newAnim : l) (runAnimations t newWorld r)
     Right _ -> runAnimations t newWorld r
 
+runAnimationsIO :: Float -> obj -> [Animation obj IO ()] -> IO (obj, [Animation obj IO ()])
+runAnimationsIO t w [] = return (w, [])
+runAnimationsIO t w (anim:r) = do
+  (newWorld, res, _) <- runAnimation anim w t
+  case res of
+    Left newAnim -> do
+      res <- runAnimationsIO t newWorld r
+      return (fmap (\l -> newAnim : l) res)
+    Right _ -> runAnimationsIO t newWorld r
+
+class Rng f where
+  rng :: (Random a) => (a, a) -> f a
+
+instance (MonadIO f) => Rng (Animation obj f) where
+  rng range = Animation $ \obj t -> do
+    v <- liftIO (randomRIO range)
+    return (obj, Right v, Just t)
+
+class Particle obj f where
+  create :: (obj -> (obj, Int)) -> f Int
+  delete :: (Int -> obj -> obj) -> Int -> f ()
+
 type RGB = (Float, Float, Float)
 
 hexToRgb :: String -> String -> String -> RGB
@@ -167,17 +194,30 @@ data Sprite = Sprite
   , _pic :: Float -> Float -> Picture
   , _width :: Float
   , _height :: Float
+  , _rotation :: Float
+  , _scale :: Float
+  , _spriteId :: Int
+  }
+
+spriteDef :: Sprite
+spriteDef = Sprite
+  { _alpha = 1
+  , _rotation = 0
+  , _scale = 1
+  , _spriteId = (-1)
   }
 
 makeLenses ''Sprite
 
 drawSprite :: Sprite -> Picture
-drawSprite Sprite{ _x, _y, _color, _alpha, _pic, _width, _height } = let
+drawSprite Sprite{ _x, _y, _color, _alpha, _pic, _width, _height, _rotation, _scale } = let
   (r, g, b) = _color
   sw = 100
   sh = 100
   in _pic _width _height &
   Color (makeColor r g b _alpha) &
+  Rotate _rotation &
+  Scale _scale _scale &
   Translate (_x + _width / 2 - sw / 2) (-_y - _height / 2 + sh / 2)
 
 data MenuWorld = MenuWorld
@@ -243,100 +283,90 @@ updateMn t w = let
 initialMenuWorld :: MenuWorld
 initialMenuWorld = MenuWorld
   { _menuBgElements =
-      [ Sprite
+      [ spriteDef
         { _x = 0
         , _y = 0
         , _width = 100
         , _height = 100
         , _color = hexToRgb "c4" "c8" "c2"
-        , _alpha = 1
         , _pic = rectangleSolid
         }
-      , Sprite
+      , spriteDef
         { _x = 0
         , _y = 0
         , _width = 100
         , _height = 28
         , _color = hexToRgb "55" "7a" "95"
-        , _alpha = 1
         , _pic = rectangleSolid
         }
-      , Sprite
+      , spriteDef
         { _x = 20
         , _y = 35
         , _width = 50
         , _height = 8
         , _color = hexToRgb "b1" "a2" "96"
-        , _alpha = 1
         , _pic = rectangleSolid
         }
-      , Sprite
+      , spriteDef
         { _x = 20
         , _y = 45
         , _width = 60
         , _height = 30
         , _color = hexToRgb "b1" "a2" "96"
-        , _alpha = 1
         , _pic = rectangleSolid
         }
-      , Sprite
+      , spriteDef
         { _x = 20
         , _y = 83
         , _width = 50
         , _height = 8
         , _color = hexToRgb "b1" "a2" "96"
-        , _alpha = 1
         , _pic = rectangleSolid
         }
-      , Sprite
+      , spriteDef
         { _x = 20
         , _y = 93
         , _width = 60
         , _height = 7
         , _color = hexToRgb "b1" "a2" "96"
-        , _alpha = 1
         , _pic = rectangleSolid
         }
-      , Sprite
+      , spriteDef
         { _x = 7.5
         , _y = 7
         , _width = 25
         , _height = 10
         , _color = hexToRgb "73" "95" "ae"
-        , _alpha = 1
         , _pic = rectangleSolid
         }
-      , Sprite
+      , spriteDef
         { _x = 37.5
         , _y = 7
         , _width = 25
         , _height = 10
         , _color = hexToRgb "73" "95" "ae"
-        , _alpha = 1
         , _pic = rectangleSolid
         }
-      , Sprite
+      , spriteDef
         { _x = 67.5
         , _y = 7
         , _width = 25
         , _height = 10
         , _color = hexToRgb "73" "95" "ae"
-        , _alpha = 1
         , _pic = rectangleSolid
         }
       ]
   , _menu =
-      Sprite
+      spriteDef
       { _x = 0
       , _y = 0
       , _width = 0
       , _height = 100
       , _color = hexToRgb "31" "55" "70"
-      , _alpha = 1
       , _pic = menuPic
       }
   , _obscuringBox =
-      Sprite
+      spriteDef
       { _x = 0
       , _y = 0
       , _width = 100
@@ -405,114 +435,195 @@ updateNb t w = let
 initialNavBarWorld :: NavBarWorld
 initialNavBarWorld = NavBarWorld
   { _navBarBgElements =
-      [ Sprite
+      [ spriteDef
         { _x = 0
         , _y = 0
         , _width = 100
         , _height = 100
         , _color = hexToRgb "c4" "c8" "c2"
-        , _alpha = 1
         , _pic = rectangleSolid
         }
-      , Sprite
+      , spriteDef
         { _x = 0
         , _y = 0
         , _width = 100
         , _height = 28
         , _color = hexToRgb "55" "7a" "95"
-        , _alpha = 1
         , _pic = rectangleSolid
         }
-      , Sprite
+      , spriteDef
         { _x = 20
         , _y = 35
         , _width = 50
         , _height = 8
         , _color = hexToRgb "b1" "a2" "96"
-        , _alpha = 1
         , _pic = rectangleSolid
         }
-      , Sprite
+      , spriteDef
         { _x = 20
         , _y = 45
         , _width = 60
         , _height = 30
         , _color = hexToRgb "b1" "a2" "96"
-        , _alpha = 1
         , _pic = rectangleSolid
         }
-      , Sprite
+      , spriteDef
         { _x = 20
         , _y = 83
         , _width = 50
         , _height = 8
         , _color = hexToRgb "b1" "a2" "96"
-        , _alpha = 1
         , _pic = rectangleSolid
         }
-      , Sprite
+      , spriteDef
         { _x = 20
         , _y = 93
         , _width = 60
         , _height = 7
         , _color = hexToRgb "b1" "a2" "96"
-        , _alpha = 1
         , _pic = rectangleSolid
         }
       ]
   , _navBarBtn1 =
-      Sprite
+      spriteDef
       { _x = 7.5
       , _y = 7
       , _width = 25
       , _height = 10
       , _color = hexToRgb "73" "95" "ae"
-      , _alpha = 1
       , _pic = rectangleSolid
       }
   , _navBarBtn2 =
-      Sprite
+      spriteDef
       { _x = 37.5
       , _y = 7
       , _width = 25
       , _height = 10
       , _color = hexToRgb "73" "95" "ae"
-      , _alpha = 1
       , _pic = rectangleSolid
       }
   , _navBarBtn3 =
-      Sprite
+      spriteDef
       { _x = 67.5
       , _y = 7
       , _width = 25
       , _height = 10
       , _color = hexToRgb "73" "95" "ae"
-      , _alpha = 1
       , _pic = rectangleSolid
       }
   , _navBarLine1 =
-      Sprite
+      spriteDef
       { _x = 7.5
       , _y = 20
       , _width = 25
       , _height = 4
       , _color = hexToRgb "5d" "5c" "61"
-      , _alpha = 1
       , _pic = rectangleSolid
       }
   , _navBarLine2 =
-      Sprite
+      spriteDef
       { _x = 37.5
       , _y = 20
       , _width = 25
       , _height = 0
       , _color = hexToRgb "5d" "5c" "61"
-      , _alpha = 1
       , _pic = rectangleSolid
       }
   , _nbAnimations = []
   , _barSelected = 1
   }
+
+data ColorWorld = ColorWorld
+  { _cwParticles :: [Sprite]
+  , _nextSpriteId :: Int
+  , _cwAnimations :: [Animation ColorWorld IO ()]
+  }
+
+makeLenses ''ColorWorld
+
+initialColorWorld :: ColorWorld
+initialColorWorld = ColorWorld
+  { _cwParticles = []
+  , _nextSpriteId = 0
+  , _cwAnimations = []
+  }
+
+drawColorWorld :: ColorWorld -> IO Picture
+drawColorWorld w = return $ Pictures (map drawSprite (w ^. cwParticles))
+
+handleInputCw :: Event -> ColorWorld -> IO ColorWorld
+handleInputCw (EventKey (MouseButton LeftButton) Down _ (mouseX, mouseY)) w =
+  return $ w & cwAnimations %~ \l -> colorAnim (mouseX + 50) (50 - mouseY) : l
+handleInputCw _ w = return w
+
+updateCw :: Float -> ColorWorld -> IO ColorWorld
+updateCw t w = do
+  (newWorld, newAnimations) <- runAnimationsIO t w (w ^. cwAnimations)
+  return $ newWorld & cwAnimations .~ newAnimations
+
+intToPic :: Int -> (Float -> Float -> Picture)
+intToPic 1 = rectangleSolid
+intToPic 2 = \w h -> circleSolid (w / 2)
+intToPic 3 = \w h -> Polygon [(0, h/2), (w/2, -h/2), (-w/2, -h/2)]
+
+createParticle :: Int -> Float -> Float -> Float -> Float -> RGB -> ColorWorld -> (ColorWorld, Int)
+createParticle ptype x y rot sc col w@(ColorWorld {_cwParticles, _nextSpriteId}) = let
+  newIndex = _nextSpriteId
+  particle = spriteDef
+      { _x = x
+      , _y = y
+      , _width = 11
+      , _height = 11
+      , _color = col
+      , _pic = intToPic ptype
+      , _spriteId = newIndex
+      , _rotation = rot
+      , _scale = sc
+      }
+  newWorld = w { _cwParticles = particle : _cwParticles, _nextSpriteId = _nextSpriteId + 1 }
+  in (newWorld, newIndex)
+
+deleteParticle :: Int -> ColorWorld -> ColorWorld
+deleteParticle id w@(ColorWorld {_cwParticles}) = let
+  newWorld = w { _cwParticles = filter (\x -> x ^. spriteId /= id) _cwParticles }
+  in newWorld
+
+instance (Applicative f) => Particle ColorWorld (Animation ColorWorld f) where
+  create f = Animation $ \obj t ->
+    let (newObj, id) = f obj
+    in pure (newObj, Right id, Just t)
+  delete f id = Animation $ \obj t ->
+    let newObj = f id obj
+    in pure (newObj, Right (), Just t)
+
+colorAnim :: (Basic ColorWorld f, Par f, Monad f, Particle ColorWorld f, Rng f) => Float -> Float -> f ()
+colorAnim x y = do
+  amt :: Int <- rng (7, 11)
+  colorAnim1 x y amt
+
+colorAnim1 :: (Basic ColorWorld f, Par f, Monad f, Particle ColorWorld f, Rng f) => Float -> Float -> Int -> f ()
+colorAnim1 x y n | n <= 0 = pure ()
+colorAnim1 x y n =
+  do
+    ptype :: Int <- rng (1, 3)
+    colorR :: Int <- rng (0, 256)
+    colorG :: Int <- rng (0, 256)
+    colorB :: Int <- rng (0, 256)
+    offsetDeg :: Float <- rng (20, 160)
+    let offset = offsetDeg / 180 * pi
+    dist :: Float <- rng (15, 45)
+    rot :: Float <- rng (0, 360)
+    sc :: Float <- rng (0.6, 1.3)
+    id <- create (createParticle ptype x y rot sc (fromIntegral colorR / 256, fromIntegral colorG / 256, fromIntegral colorB / 256))
+    colorAnim2 x y offset dist id
+    delete deleteParticle id
+  `par` colorAnim1 x y (n - 1)
+
+colorAnim2 :: (Basic ColorWorld f, Par f) => Float -> Float -> Float -> Float -> Int -> f ()
+colorAnim2 mx my offset dist id =
+        basic (cwParticles . withSpriteId id . x) (For 1) (To (mx + dist * cos offset))
+  `par` basic (cwParticles . withSpriteId id . y) (For 1) (To (my - dist * sin offset))
+  `par` basic (cwParticles . withSpriteId id . alpha) (For 1) (To 0.3)
 
 fetchInbetweens :: Float -> Int -> obj -> Animation obj Identity a -> [obj] -> [obj]
 fetchInbetweens _ 0 obj _ acc = acc ++ [obj]
@@ -585,6 +696,7 @@ main = let
   case x of
     1 -> play window bgColor 60 initialNavBarWorld drawNavBarWorld handleInputNb updateNb
     2 -> play window bgColor 60 initialMenuWorld drawMenuWorld handleInputMn updateMn
+    3 -> playIO window bgColor 60 initialColorWorld drawColorWorld handleInputCw updateCw
     9 -> do
       exportPictureToPNG (sw * 4 + 4 * 3, sh + 20) bgColor "../paper/pictures/usecase1fig.png" (drawnWorlds drawMenuWorld usecase1fig)
       exportPictureToPNG (sw * 4 + 4 * 3, sh + 20) bgColor "../paper/pictures/usecase1basic1.png" (drawnWorlds drawMenuWorld usecase1basic1)
@@ -593,3 +705,16 @@ main = let
       exportPictureToPNG (sw * 4 + 4 * 3, sh + 20) bgColor "../paper/pictures/usecase2basic1.png" (drawnWorlds drawNavBarWorld usecase2basic1)
       exportPictureToPNG (sw * 4 + 4 * 3, sh + 20) bgColor "../paper/pictures/usecase2basic2.png" (drawnWorlds drawNavBarWorld usecase2basic2)
     _ -> error "unknown"
+
+atIndex :: Int -> Lens' [a] a
+atIndex i = lens (!! i) (\s b -> take i s ++ b : drop (i+1) s)
+
+withSpriteId :: Int -> Lens' [Sprite] Sprite
+withSpriteId i = let
+  get l = case find (\x -> x ^. spriteId == i) l of
+    Just s -> s
+    Nothing -> error ("no particle with index " ++ show i)
+  set l x = case findIndex (\x -> x ^. spriteId == i) l of
+    Just ix -> take ix l ++ x : drop (ix+1) l
+    Nothing -> error ("no particle with index " ++ show i)
+  in lens get set
